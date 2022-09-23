2d760e2103703c271e6ddfbc030eb6ba5fd510d9
usmanino <usmaninobello@gmail.com>
2022-09-23 22:37:45 +0100

    first committ

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import Hero from "./components/Hero";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Splashpage from "./components/splash";
 
 const App = () => {
   return (
     <>
         <Header />
-        <Splashpage />
+        <Hero />
         <Footer />
     </>
   );
